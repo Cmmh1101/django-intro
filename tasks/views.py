1df03533ab1bb6eaddc97e3990e80299fcaf916f
Carla Montano <cm.emprendiendo@gmail.com>
2023-02-05 23:51:31 -0600

    save data and redirect if valid

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,13 @@
 from django import forms
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
-tasks = ["read", "cook", "workout"]
+tasks = []
 
 
 class NewTaskForm(forms.Form):
-    taks = forms.CharField(label="New Task")
-    priority = forms.IntegerField(
-        label="priority", min_value=1, max_value="10")
+    task = forms.CharField(label="New Task")
 
 # Create your views here.
 
@@ -22,16 +22,17 @@ def add(request):
     # check for post method to access entered form data
     if request.method == "POST":
         form = NewTaskForm(request.POST)
-    if form.is_valid():
-        # if form is valid save data in task variable
-        task = form.cleaned_data["task"]
-        # add taks to the list of tasks
-        tasks.append(task)
-    else:
-        # not valid? return to the form, with the entered data giving error feedback
-        return render(request, "tasks/add.html", {
-            "form": form
-        })
+        if form.is_valid():
+            # if form is valid save data in task variable
+            task = form.cleaned_data["task"]
+            # add taks to the list of tasks
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            # not valid? return to the form, with the entered data giving error feedback
+            return render(request, "tasks/add.html", {
+                "form": form
+            })
 
     return render(request, 'tasks/add.html', {
         "form": NewTaskForm()
