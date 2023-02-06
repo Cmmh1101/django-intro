3229b0606d459e80a99cbc66b302303f219a0d50
Carla Montano <cm.emprendiendo@gmail.com>
2023-02-05 22:20:44 -0600

    looping tasks

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+tasks = ["read", "cook", "workout"]
+
 # Create your views here.
+
+
+def index(request):
+    return render(request, "tasks/index.html", {
+        "tasks": tasks
+    })
